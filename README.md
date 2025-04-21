e929b173ac6e779e49aad8c94356f03754a7a414
yeswanth274 <samuelpola796@gmail.com>
2025-04-21 09:51:52 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Lab Exercise: Deciphering SHA256 Hash of a 3-Digit Number
+
+## 🎯 Objectives
+
+- Grasp the concept of SHA256 cryptographic hashing.
+- Enhance analytical skills in decoding hash values.
+
+## 🔍 Prerequisites
+
+- Basic understanding of hashes and number systems.
+
+## 🚀 Steps
+
+1. **Learn About SHA256**: Familiarize yourself with the SHA256 hashing algorithm and its use cases.
+
+2. **Access the Interface**: Navigate to the provided browser for hash cracking.
+
+3. **Analyze the Given SHA256 Hash**: A unique SHA256 hash value of a 3-digit number will be displayed on your screen.
+
+4. **Deductive Thinking**: Use your knowledge and logical reasoning to guess the original 3-digit number.
+
+5. **Input Your Guess**: Enter your 3-digit number guess in the provided input field.
+
+6. **Check Your Answer**: Click the 'Check' button to see if your guess decrypts the hash correctly.
+
+7. **Iterate and Learn**: If unsuccessful, analyze your approach, learn from it, and try again!
+
+## 🏁 Outcome
+- Mastery in interpreting SHA256 hashes through critical thinking and analysis.
---
+++ script.js
@@ -0,0 +1,94 @@
+const MIN = 100;
+const MAX = 999;
+const pinInput = document.getElementById('pin');
+const sha256HashView = document.getElementById('sha256-hash');
+const resultView = document.getElementById('result');
+
+// a function to store in the local storage
+function store(key, value) {
+  localStorage.setItem(key, value);
+}
+
+// a function to retrieve from the local storage
+function retrieve(key) {
+  return localStorage.getItem(key);
+}
+
+function getRandomArbitrary(min, max) {
+  let cached;
+  cached = Math.random() * (max - min) + min;
+  cached = Math.floor(cached);
+  return cached;
+}
+
+// a function to clear the local storage
+function clear() {
+  localStorage.clear();
+}
+
+// a function to generate sha256 hash of the given string
+async function sha256(message) {
+  // encode as UTF-8
+  const msgBuffer = new TextEncoder().encode(message);
+
+  // hash the message
+  const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
+
+  // convert ArrayBuffer to Array
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+
+  // convert bytes to hex string
+  const hashHex = hashArray
+    .map((b) => b.toString(16).padStart(2, '0'))
+    .join('');
+  return hashHex;
+}
+
+async function getSHA256Hash() {
+  let cached = retrieve('sha256');
+  if (cached) {
+    return cached;
+  }
+
+  cached = await sha256(getRandomArbitrary(MIN, MAX));
+  store('sha256', cached);
+  return cached;
+}
+
+async function main() {
+  sha256HashView.innerHTML = 'Calculating...';
+  const hash = await getSHA256Hash();
+  sha256HashView.innerHTML = hash;
+}
+
+async function test() {
+  const pin = pinInput.value;
+
+  if (pin.length !== 3) {
+    resultView.innerHTML = '💡 not 3 digits';
+    resultView.classList.remove('hidden');
+    return;
+  }
+
+  const sha256HashView = document.getElementById('sha256-hash');
+  const hasedPin = await sha256(pin);
+
+  if (hasedPin === sha256HashView.innerHTML) {
+    resultView.innerHTML = '🎉 success';
+    resultView.classList.add('success');
+  } else {
+    resultView.innerHTML = '❌ failed';
+  }
+  resultView.classList.remove('hidden');
+}
+
+// ensure pinInput only accepts numbers and is 3 digits long
+pinInput.addEventListener('input', (e) => {
+  const { value } = e.target;
+  pinInput.value = value.replace(/\D/g, '').slice(0, 3);
+});
+
+// attach the test function to the button
+document.getElementById('check').addEventListener('click', test);
+
+main();
